0ad87e55b6e8521333a8b7094b67be9d6bc1fae1
cueltschey <chaseuelt@gmail.com>
2023-11-10 18:16:56 -0600

    make basic outline of user class

--- User.py
+++ User.py
@@ -1,3 +1,22 @@
 import sqlite3
 
-print("hello world")
+class User:
+    def __init__(self):
+        self.databaseName = ""
+        self.tableName = ""
+    def __init__(self, databse, table):
+        self.databaseName = database
+        self.tableName = table
+
+    def viewCart(self, userID, inventoryDatabase):
+        # get and display books in the users cart
+
+    def addToCart(userID, ISBN):
+        # add a new book or increment existing book
+
+    def removeFromCart(userID, ISBN):
+        # remove all books of matching ISBN
+
+    def checkOut(userID):
+        # clear cart and call inventory checkout
+
