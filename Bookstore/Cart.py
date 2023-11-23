95b87e7a25e68cf7640e5d6f3f17e96f71eb6753
Amit Gupta <116447781+Amit0200@users.noreply.github.com>
2023-11-23 15:42:05 -0600

    Cart.py

--- Bookstore/Cart.py
+++ Bookstore/Cart.py
@@ -2,36 +2,35 @@ import sqlite3
 import os
 
 class Cart:
-    def __init__(self, user_id, database_name="MainDatabase.db"):
-        self.cart_items = {}
-        self.user_id = user_id
-        self.database_name = database_name
+    def __init__(self, userID, database="Bookstore.db"):
+        self.cartItems = {}
+        self.userID = userID
+        self.database = database
 
-    def add_book(self, book_isbn, quantity):
-        if book_isbn in self.cart_items:
-            self.cart_items[book_isbn] += quantity
+    def addBook(self, ISBN, quantity):
+        if ISBN in self.cartItems:
+            self.cartItems[ISBN] += quantity
         else:
-            self.cart_items[book_isbn] = quantity
+            self.cartItems[ISBN] = quantity
 
-    def remove_book(self, book_isbn):
-        if book_isbn in self.cart_items:
-            if self.cart_items[book_isbn] > 1:
-                self.cart_items[book_isbn] -= 1
+    def removeBook(self, ISBN):
+        if ISBN in self.cartItems:
+            if self.cartItems[ISBN] > 1:
+                self.cartItems[ISBN] -= 1
             else:
-                del self.cart_items[book_isbn]
+                del self.cartItems[ISBN]
 
-    def checkout(self):
-        # Implement logic to create an order and return it after payment is accepted
+    def checkOut(self):
         pass
 
-    def view_cart(self):
-        connection = sqlite3.connect(self.database_name)
+    def viewCart(self):
+        connection = sqlite3.connect(self.database)
         cursor = connection.cursor()
 
         print("Shopping Cart:")
         print("ISBN\tQuantity")
 
-        for isbn, qty in self.cart_items.items():
+        for isbn, qty in self.cartItems.items():
             print(f"{isbn}\t{qty}")
 
         connection.close()
