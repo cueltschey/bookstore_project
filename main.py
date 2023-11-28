bb4838dfbe2a531d5f3380b5076831a0ce0331e5
cueltschey <chaseuelt@gmail.com>
2023-11-28 17:06:58 -0600

    testing

--- main.py
+++ main.py
@@ -12,21 +12,35 @@ print("2. Register a new account.")
 print("3. Logout.")
 option = input("Input option here: ")
 
-while(option[0] not in '123' and user.loggenIn == False):
+while(option not in '123' or user.loggedIn == False):
+    if(option == '1'):
+        user.login()
+    elif(option == '2'):
+        user.createAccount()
+    elif(option == '3'):
+        user.logout()
+
+    if(user.loggedIn):
+        break
+
     print("1. Login to existing account.")
     print("2. Register a new account.")
     print("3. Logout.")
     option = input("incorrect, re-enter option: ")
-    if(option[0] == '1'):
-        user.login()
-    elif(option[0] == '2'):
-        user.createAccount()
-    elif(option[0] == '3'):
-        user.logout()
+
+pos = "main"
 
 while(user.loggedIn):
-    print("inside")
-    break
+    if(pos == "main"):
+        print("Main Menu:", "\n")
+        print("1. Logout")
+        print("2. Account Information")
+        print("3. Inventory Information")
+        print("4. Cart Information")
+        
+        option = "none"
+        while(option[0] not in "1234"):
+            option = input("Input option: ")
 
 
 
