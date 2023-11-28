37142375d60f76c1a725bea3ca70d7858f904465
cueltschey <chaseuelt@gmail.com>
2023-11-28 16:46:03 -0600

    test

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from Bookstore.User import User
 from Bookstore.Inventory import Inventory
 from Bookstore.Cart import Cart
 
-# testing for User
 
-c = Cart()
+user = User("Bookstore/User.db", "User")
+inventory = Inventory("Bookstore/Inventory.db", "Inventory")
+print("Login Page:", "\n")
 
-c.viewCart(1)
+print("1. Login to existing account.")
+print("2. Register a new account.")
+option = input("Input option here: ")
 
-# end testing for User
+option = int(option)
+
+while(not user.login()):
+    print("Please try again...")
+
+while(user.loggedIn):
+    print("inside")
+    break
+
+
+
+
+
