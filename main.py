7f1ce14e22fce5243d0c0b31492a93f1f5a02e72
cueltschey <chaseuelt@gmail.com>
2023-11-28 16:55:56 -0600

    inter

--- main.py
+++ main.py
@@ -9,12 +9,20 @@ print("Login Page:", "\n")
 
 print("1. Login to existing account.")
 print("2. Register a new account.")
+print("3. Logout.")
 option = input("Input option here: ")
 
-option = int(option)
-
-while(not user.login()):
-    print("Please try again...")
+while(option[0] not in '123' and user.loggenIn == False):
+    print("1. Login to existing account.")
+    print("2. Register a new account.")
+    print("3. Logout.")
+    option = input("incorrect, re-enter option: ")
+    if(option[0] == '1'):
+        user.login()
+    elif(option[0] == '2'):
+        user.createAccount()
+    elif(option[0] == '3'):
+        user.logout()
 
 while(user.loggedIn):
     print("inside")
